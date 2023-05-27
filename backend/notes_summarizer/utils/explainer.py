86d34cd1018a78670f98c57a709b5002d9eab18f
Zak Raicik <raicik.zach@Gmail.com>
2023-05-27 09:02:34 -0700

    Added basic error handling

--- backend/notes_summarizer/utils/explainer.py
+++ backend/notes_summarizer/utils/explainer.py
@@ -1,18 +1,35 @@
 import openai
+import openai.error
+import logging
+from openai.error import AuthenticationError, RateLimitError
 
+logging.basicConfig(filename="error.log", level=logging.ERROR)
 
 system_msg = "You are a helpful assistant who can explain how code works."
 
 
 def explainCode(api_key, code):
     openai.api_key = api_key
-    code = str(code)
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        messages=[
-            {"role": "system", "content": system_msg},
-            {"role": "user", "content": code},
-        ],
-    )
 
-    return response
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": f"{system_msg}",
+                },
+                {"role": "user", "content": f"{code}"},
+            ],
+        )
+
+        return response
+    except AuthenticationError as e:
+        logging.error(f"AuthenticationError: {str(e)}")
+        raise e
+    except RateLimitError as e:
+        logging.error(f"RateLimitError: {str(e)}")
+        raise e
+    except Exception as e:
+        logging.error(f"Unexpected error: {str(e)}")
+        raise e
